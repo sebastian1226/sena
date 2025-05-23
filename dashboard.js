cfa98d782655a70f8db9451e735d1587274b783a
sebastian1226 <sebastiantoribiodavila@gmail.com>
2025-05-23 17:05:54 -0500

    Update dashboard.js
    cambio de puerto

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,5 @@
 // URL base de la API
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'http://localhost:3002/api';
 
 function obtenerFechaActualFormateada() {
   const hoy = new Date();
